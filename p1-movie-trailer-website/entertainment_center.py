2c050a525bc0bd192b3996057858b6a71dc3a7f5
Philip Gura <philipgura@gmail.com>
2016-03-22 10:26:00 -0500

    Clean up

--- p1-movie-trailer-website/entertainment_center.py
+++ p1-movie-trailer-website/entertainment_center.py
@@ -13,8 +13,6 @@ movies.append(media.Movie("Zoolander", "http://www.gstatic.com/tv/thumb/moviepos
 #build html page
 fresh_tomatoes.open_movies_page(movies)
 
-#print(media.Movie.VALID_RATINGS)
-
 print(media.Movie.__doc__)
 print(media.Movie.__name__)
 print(media.Movie.__module__)
